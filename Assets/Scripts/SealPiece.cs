3fd53e069de2a7f85236e592130568234a0332ee
desdemonhu <desdemonhu@gmail.com>
2021-04-13 22:54:39 -0700

    Working in Finding Neighbors - can check if a piece is valad

--- Assets/Scripts/SealPiece.cs
+++ Assets/Scripts/SealPiece.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c5025a17ddcfac1273867cb6d254dea028595f0d872493ae4774b08fba309a7e
-size 1424
+oid sha256:e14d12b4379a0c596a92ae002c2f51281d4b1e0308d6f0e6229f5c7cfaa33803
+size 6615
